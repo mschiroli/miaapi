f720fa9c6043c63e7b976f038987463c317b6291
Schiroli Massimiliano <m.schiroli@itcity.it>
2026-05-04 12:00:12 +0200

    Modify weatherforecast output to test GitHub Actions

--- Program.cs
+++ Program.cs
@@ -21,18 +21,22 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = "Modified by CI/CD",
+            Source = "GitHub Actions"
+        })
         .ToArray();
-    return forecast;
-})
+
+    return Results.Ok(forecast);
+});
+
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
